c4369ace4faffe3bcf9a29251a8de7ac59a6ca7c
Jannek Squar <squar@informatik.uni-hamburg.de>
2018-07-31 16:41:27 +0200

    Package weiter aufgeräumt

--- var/spack/repos/builtin/packages/saga-gis/package.py
+++ var/spack/repos/builtin/packages/saga-gis/package.py
@@ -22,95 +22,69 @@
 # License along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 ##############################################################################
-#
-# This is a template package file for Spack.  We've put "FIXME"
-# next to all the things you'll want to change. Once you've handled
-# them, you can save this file and test your package like this:
-#
-#     spack install saga-gis
-#
-# You can edit this file again by typing:
-#
-#     spack edit saga-gis
-#
-# See the Spack documentation for more information on packaging.
-# If you submit this package back to Spack as a pull request,
-# please first remove this boilerplate and all FIXME comments.
-#
 from spack import *
 
 
 class SagaGis(AutotoolsPackage):
-    """FIXME: Put a proper description of your package here."""
+    """
+    SAGA is a GIS for Automated Geoscientific Analyses and has been designed for an
+    easy and effective implementation of spatial algorithms. It offers a comprehensive,
+    growing set of geoscientific methods and provides an easily approachable user
+    interface with many visualisation options
+    """
 
     homepage = "http://saga-gis.org/"
-#FIXME: Url noch nicht ausreichend für automatischen Download
-#    url      = "https://sourceforge.net/projects/saga-gis/files/SAGA%20-%205/SAGA%20-%205.0.0/saga-5.0.0.tar.gz"
+    url      = "https://sourceforge.net/projects/saga-gis/files/SAGA%20-%205/SAGA%20-%205.0.0/saga-5.0.0.tar.gz"
 #    git      = "https://git.code.sf.net/p/saga-gis/code.git"
 #
 #
 #    version('develop', branch='master')
 #    version('6.0.0', branch='release-6.0.0')
 #    version('5.0.1', branch='release-5-0-1')
-#    version('5.0.0', '475adff7b2e05cbdf5ccabfbbca449ac')
-#
-#    variant('shared',   default=True,   description='Build shared libraries')
-#    variant('static',   default=True,   description='Build static libraries')
-##FIXME: Debug Variant notwendig?
-#    #variant('debug',    default=False,  description='Turn on debugging')
-#    variant('gui',      default=True,   description='Build GUI and interactive SAGA tools')
-#    variant('odbc',     default=True,   description='Build with ODBC support')
-#    variant('triangle', default=True,   description='Build with triangle.c (non free for commercial use); otherwise use qhull')
-#    variant('libfire',  default=True,   description='Build with libfire (non free for commercial usage)')
-#    variant('openmp',   default=True,   description='Build with OpenMP enabled')
-#    variant('python',   default=False,  description='Build Python extension')
-#
-#    depends_on('autoconf', type='build')
-#    depends_on('automake', type='build')
-#    depends_on('libtool',  type='build')
-#    depends_on('m4',       type='build')
-#
-#    depends_on('wx')
-#    depends_on('gdal')
-#    depends_on('unixodbc')
-#    depends_on('proj')
-#    depends_on('qhull', when='~triangle')
+    version('5.0.0', '475adff7b2e05cbdf5ccabfbbca449ac')
+
+    variant('gui',      default=True,   description='Build GUI and interactive SAGA tools')
+    variant('odbc',     default=True,   description='Build with ODBC support')
+    variant('triangle', default=True,   description='Build with triangle.c non free for commercial use otherwise use qhull')
+    variant('libfire',  default=True,   description='Build with libfire (non free for commercial usage)')
+    variant('openmp',   default=True,   description='Build with OpenMP enabled')
+    variant('python',   default=False,  description='Build Python extension')
+
+    depends_on('autoconf', type='build')
+    depends_on('automake', type='build')
+    depends_on('libtool',  type='build')
+    depends_on('m4',       type='build')
+
+    depends_on('wx')
+    depends_on('gdal')
+    depends_on('unixodbc')
+    depends_on('proj')
+
+    depends_on('qhull', when='~triangle')
+    depends_on('swig', type='build', when='+python')
 
     def autoreconf(self, spec, prefix):
-        # FIXME: Modify the autoreconf method as necessary
         autoreconf('--install', '--verbose', '--force')
 
-#    def configure_args(self):
-#        # FIXME: Add arguments other than --prefix
-#        # FIXME: If not needed delete this function
-#        args = []
-#        if spec.satisfies('@5:'):
-#            if '+shared' in spec:
-#                args.append('--enable-shared=yes')
-#            else:
-#                args.append('--enable-shared=no')
-#
-#            if '+static' in spec:
-#                args.append('--enable-static=yes')
-#            else:
-#                args.append('--enable-static=no')
-#
-#            if '~gui' in spec:
-#                args.append('--disable-gui')
-#
-#            if '~odbc' in spec:
-#                args.append('--disable-odbc')
-#
-#            if '~triangle' in spec:
-#                args.append('--disable-triangle')
-#
-#            if '~libfire' in spec:
-#                args.append('--disable-libfire')
-#
-#            if '~openmpi' in spec:
-#                args.append('--disable-openmp')
-#
-#            if '+python' in spec:
-#                args.append('--enable-python')
-#
-#        return args
+    def configure_args(self):
+        args = []
+        if self.spec.satisfies('@5:'):
+            if '~gui' in self.spec:
+                args.append('--disable-gui')
+
+            if '~odbc' in self.spec:
+                args.append('--disable-odbc')
+
+            if '~triangle' in self.spec:
+                args.append('--disable-triangle')
+
+            if '~libfire' in self.spec:
+                args.append('--disable-libfire')
+
+            if '~openmpi' in self.spec:
+                args.append('--disable-openmp')
+
+            if '+python' in self.spec:
+                args.append('--enable-python')
+
+        return args
